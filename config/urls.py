9a28e04b5db5ce5421894f83db5cdee42d6d795e
roberto7744 <robertoes.garret@gmail.com>
2025-12-08 01:41:54 -0300

    remove admin creation script

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from .views import index
-from store import create_admin
+#from store import create_admin
 
 
 schema_view = get_schema_view(
